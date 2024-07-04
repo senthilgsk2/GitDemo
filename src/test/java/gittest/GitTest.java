e2969b4b724ae7b355b9d3ab1ef3e225e17dbd7a
senthil <mgsenthilmgs@gmail.com>
2024-07-04 21:36:50 +0530

    first commit

---
+++ src/test/java/gittest/GitTest.java
@@ -0,0 +1,17 @@
+package gittest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("www.google.com");
+	}
+
+}
